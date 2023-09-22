76d6e45ea91c0841146680d61108fcfe4b0a394a
Yu SERIZAWA <upamune@gmail.com>
2023-09-22 16:25:52 +0900

    init

---
+++ cmd/podcastserver/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	httpstd "net/http"
+	"os"
+	"time"
+
+	"github.com/rs/zerolog"
+
+	"github.com/upamune/podcast-server/http"
+	"github.com/upamune/podcast-server/podcast"
+)
+
+func main() {
+	os.Exit(realMain())
+}
+
+func realMain() int {
+	baseURL := flag.String("baseurl", "", "base URL of server")
+	targetDir := flag.String("targetdir", "", "audio target directory")
+	basicAuth := flag.String("basicauth", "", "basic auth for HTTP server")
+	flag.Parse()
+
+	if baseURL == nil || *baseURL == "" {
+		fmt.Fprintf(os.Stderr, "-baseurl is required")
+		return 1
+	}
+
+	if targetDir == nil || *targetDir == "" {
+		fmt.Fprintf(os.Stderr, "-targetdir is required")
+		return 1
+	}
+
+	logger := zerolog.New(os.Stderr)
+
+	now := time.Now()
+	podcaster := podcast.NewPodcaster(
+		logger,
+		*baseURL,
+		*targetDir,
+		"Radiko",
+		*baseURL,
+		"Radiko",
+		&now,
+	)
+
+	if err := podcaster.Sync(); err != nil {
+		logger.Error().Err(err).Msg("failed to initial sync")
+		return 1
+	}
+	handler, err := http.NewHTTPHandler(podcaster, *targetDir, *basicAuth)
+	if err != nil {
+		logger.Error().Err(err).Msg("failed to create HTTP handler")
+		return 1
+	}
+
+	httpstd.ListenAndServe(":3333", handler)
+
+	return 0
+}
---
+++ go.mod
@@ -0,0 +1,23 @@
+module github.com/upamune/podcast-server
+
+go 1.21
+
+require (
+	github.com/eduncan911/podcast v1.4.2
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/rs/zerolog v1.21.0
+)
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
+	golang.org/x/net v0.0.0-20210423184538-5f58ad60dda6 // indirect
+	golang.org/x/sys v0.0.0-20210423185535-09eb48e85fd7 // indirect
+	golang.org/x/text v0.3.6 // indirect
+)
